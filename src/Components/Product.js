fad20360e02ae4cc74f8dc6a5245000eeb44c6e1
tochibedford <musicofbyte@gmail.com>
2022-05-24 23:57:37 +0100

    🍗🍗fleshed the Product.js, added "add to cart"

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,10 +1,56 @@
-import { Component } from 'react'
+import { Component, createRef } from 'react'
+import cart from '../images/cart.svg'
+import productImage from '../images/Smino.jpg'
 
 export default class Product extends Component{
+    constructor(props){
+        super(props);
+        
+        this.addToCartRef = createRef()
+        this.handleMouseOver = this.handleMouseOver.bind(this)
+        this.handleMouseLeave = this.handleMouseLeave.bind(this)
+        this.state = {
+            showAddToCart: false 
+        }
+    }
+
+    handleMouseOver(){
+        this.addToCartRef.current.style.opacity = 1;
+        this.setState(prevState=>{
+            return(
+                {
+                    showAddToCart: true
+                }
+            )
+        })
+    }
+
+    handleMouseLeave(){
+        this.addToCartRef.current.style.opacity = 0;
+        this.setState(prevState=>{
+            return(
+                {
+                    showAddToCart: true
+                }
+            )
+        })
+    }
+
 
     render(){
         return(
-            <div className="product"></div>
+            <div className="product" onMouseOver={this.handleMouseOver} onMouseLeave={this.handleMouseLeave}>
+                <div className="productImageContainer">
+                    <img className="productImage" src={productImage} alt="product" />
+                    <div className="addToCartImageContainer" ref={this.addToCartRef}>
+                        <img className="addToCartImage" src={cart} alt="add to cart" />
+                    </div>
+                </div>
+                <div className="productInfo">
+                    <div className="productName">Running Shorts</div>
+                    <div className="productPrice">$50.00</div>
+                </div>
+            </div>
         )
     }
 }
