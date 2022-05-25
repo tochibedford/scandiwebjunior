bb672736032adf92664a59c5b62d2ac127fc51e2
tochibedford <musicofbyte@gmail.com>
2022-05-25 01:08:55 +0100

    🍦switch "addToCart" opacity control to CSS 🍦🍦

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -3,46 +3,13 @@ import cart from '../images/cart.svg'
 import productImage from '../images/Smino.jpg'
 
 export default class Product extends Component{
-    constructor(props){
-        super(props);
-        
-        this.addToCartRef = createRef()
-        this.handleMouseOver = this.handleMouseOver.bind(this)
-        this.handleMouseLeave = this.handleMouseLeave.bind(this)
-        this.state = {
-            showAddToCart: false 
-        }
-    }
-
-    handleMouseOver(){
-        this.addToCartRef.current.style.opacity = 1;
-        this.setState(prevState=>{
-            return(
-                {
-                    showAddToCart: true
-                }
-            )
-        })
-    }
-
-    handleMouseLeave(){
-        this.addToCartRef.current.style.opacity = 0;
-        this.setState(prevState=>{
-            return(
-                {
-                    showAddToCart: true
-                }
-            )
-        })
-    }
-
 
     render(){
         return(
-            <div className="product" onMouseOver={this.handleMouseOver} onMouseLeave={this.handleMouseLeave}>
+            <div className="product" >
                 <div className="productImageContainer">
                     <img className="productImage" src={productImage} alt="product" />
-                    <div className="addToCartImageContainer" ref={this.addToCartRef}>
+                    <div className="addToCartImageContainer">
                         <img className="addToCartImage" src={cart} alt="add to cart" />
                     </div>
                 </div>
