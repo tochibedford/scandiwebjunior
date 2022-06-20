882c30fe9fdfc374fefd47f72f1d5429b3d3ca98
Tochukwu Bedford <34871260+tochibedford@users.noreply.github.com>
2022-06-20 01:39:45 +0100

    fixing cors issue with CORS anywhere

--- src/Components/helpers.js
+++ src/Components/helpers.js
@@ -6,7 +6,7 @@ export const graphFetch = async(query) => {
             query: query
         })
     }
-    let data = await fetch('https://mockstore-endpoint.herokuapp.com/', fetchOptions)
+    let data = await fetch('https://tochi-cors-anywhere.herokuapp.com/https://mockstore-endpoint.herokuapp.com/', fetchOptions)
     data = await data.json();
         
     return data.data
